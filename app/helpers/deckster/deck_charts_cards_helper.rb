e30fe132a6265f4af83ea082d876ece1243d1548
Greg Hochard <ghochard@42six.com>
2015-01-06 14:40:16 -0500

    D3 Dendrogram

--- app/helpers/deckster/deck_charts_cards_helper.rb
+++ app/helpers/deckster/deck_charts_cards_helper.rb
@@ -3,5 +3,13 @@ module Deckster
     def render_deckster_bubble_chart_card chart_configs
       render partial: "deckster/chart_cards/bubble_chart_card", locals: {chart_configs: chart_configs}
     end
+
+    def render_deckster_cluster_dendrogram_tree_card chart_configs
+      render partial: "deckster/chart_cards/cluster_dendrogram_tree", locals: {chart_configs: chart_configs}
+    end
+
+    def render_deckster_cluster_dendrogram_radial_card chart_configs
+      render partial: "deckster/chart_cards/cluster_dendrogram_radial", locals: {chart_configs: chart_configs}
+    end
   end
 end
